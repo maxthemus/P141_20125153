321d5fbee14ab3c4e460566f7b0fde32d44c8ebd
Max Somerville <max.somerville62@gmail.com>
2022-06-02 13:22:36 +1200

    DB commit
    first db commit

---
+++ src/main/java/com/max/p141_20125153_assignment2/DB_Controller.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.max.p141_20125153_assignment2;
+
+/**
+ *
+ * @author Maxth
+ */
+public class DB_Controller {
+    //Fields
+    
+    //Constructor
+    
+    //Methods
+    
+}
